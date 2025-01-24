1c4420165d948d089c5251da35a7b4d915dde733
Chandan <cy2001435@gmail.com>
2025-01-24 18:25:52 +0530

    added review modal and right section in component

--- src/pages/exam_management/AddQuestion.jsx
+++ src/pages/exam_management/AddQuestion.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Tabs, Select, Button, Input, Row, Col, Space, Checkbox } from "antd";
-import { PlusOutlined, UploadOutlined, BankOutlined } from "@ant-design/icons";
+import { Row, Col } from "antd";
 import styled from "styled-components";
 import ShortType from "../questionType/ShortType";
-
-const { TabPane } = Tabs;
+import RightSection from "./RightSection";
+import CommonModalComponent from "../../components/CommonModalComponent";
+import ButtonComponent from "../../exam_components/button_component/ButtonComponent";
+import ReviewModal from "./ReviewModal";
 
 const QuestionGrid = styled.div`
   display: grid;
@@ -49,6 +50,7 @@ const AddQuestion = () => {
   const [activeSection, setActiveSection] = useState(1);
   const [questionType, setQuestionType] = useState("Short Answer Type");
   const [marks, setMarks] = useState(0);
+  const [isReviewModalOpen, setReviewModalOpen] = useState(false);
 
   const handleMarksChange = (e) => {
     const value = e.target.value; // Get the input value as a string
@@ -66,7 +68,7 @@ const AddQuestion = () => {
 
   const handleTabChange = (key) => setActiveSection(Number(key));
 
-  const renderQuestionGrid = (section) => (
+  const renderQuestionGrid = (section, sectionIndex) => (
     <div key={section.key} style={{ marginBottom: "24px" }}>
       <div className="d-flex justify-content-between mb12">
         <div className="label-20-500-b">{section.name}</div>
@@ -74,7 +76,10 @@ const AddQuestion = () => {
       </div>
       <QuestionGrid>
         {Array.from({ length: section.questions }, (_, i) => (
-          <QuestionBox key={i}>{i + 1}</QuestionBox>
+          <QuestionBox key={i}>
+            {" "}
+            {i + 1 + sectionIndex * section.questions}
+          </QuestionBox>
         ))}
       </QuestionGrid>
     </div>
@@ -82,154 +87,43 @@ const AddQuestion = () => {
 
   return (
     <div>
+      <ButtonComponent
+        label="Review Modal"
+        gradient={true}
+        buttonActionType="create"
+        onClick={() => setReviewModalOpen(true)}
+      />
       <div style={{ marginBottom: "24px" }}>
+        {/* <CommonModalComponent /> */}
         <div className="label-28-600">Summative Assessment - I</div>
         <div className="label-14-600-blue">View Examination Details</div>
       </div>
       <Row gutter={[24, 24]}>
         {/* Left Section */}
         <Col xs={24} md={16}>
-          <Tabs
-            activeKey={String(activeSection)} // Ensure this is a string matching the `key`
-            onChange={(key) => setActiveSection(Number(key))}
-            items={sections.map((section) => ({
-              key: String(section.key), // Ensure key is a string
-              label: (
-                <span
-                  className={
-                    activeSection === section.key
-                      ? "label-20-600"
-                      : "label-20-500"
-                  }
-                  style={{
-                    color:
-                      activeSection === section.key ? "#4B4B4B" : "#797979",
-                  }}
-                >
-                  {section.name}
-                </span>
-              ),
-              children: (
-                <>
-                  <ShortType />
-                  {/* <Space
-                    direction="vertical"
-                    style={{ width: "100%" }}
-                    size="large"
-                  >
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        gap: "16px",
-                      }}
-                    >
-                      <Checkbox className="label-18-500-b">Question 1</Checkbox>
-                      <Select
-                        value={questionType}
-                        onChange={setQuestionType}
-                        style={{
-                          display: "flex",
-                          width: "363px",
-                          height: "44px",
-                          alignItems: "center",
-                          gap: "139px",
-                          flexShrink: 0,
-                          borderRadius: "8px",
-                          border: "1px solid #797979",
-                        }}
-                      >
-                        {questionTypes.map((type) => (
-                          <Select.Option key={type} value={type}>
-                            {type}
-                          </Select.Option>
-                        ))}
-                      </Select>
-                      <div
-                        style={{
-                          display: "flex",
-                          alignItems: "center",
-                          gap: "8px",
-                        }}
-                      >
-                        <Input
-                          min={0}
-                          max={20}
-                          value={marks}
-                          onChange={handleMarksChange}
-                          style={{ width: 50 }}
-                        />
-                        <span className="label-16-500-g-i">Marks</span>
-                      </div>
-                    </div>
-                    <Input.TextArea
-                      placeholder="Type the question here"
-                      rows={4}
-                    />
-                    <Button type="link" icon={<PlusOutlined />}>
-                      Add a New Question
-                    </Button>
-                  </Space> */}
-                </>
-              ),
-            }))}
-          />
+          <ShortType />
         </Col>
 
         {/* Right Section */}
         <Col xs={24} md={8}>
-          <div
-            className="left-container-question-create"
-            style={{ padding: "37px 36px" }}
-          >
-            {sections.map(renderQuestionGrid)}
-            <Space direction="vertical" size="middle" style={{ width: "100%" }}>
-              <div className="d-flex justify-content-between gap11">
-                <Button type="primary" className="btn-review">
-                  Review Paper
-                </Button>
-                <Button className="btn-save-draft">Save as Draft</Button>
-              </div>
-              <div className="diflex gap-3 mb20">
-                <div className="d-flex align-items-center gap-3 mb20">
-                  <PlusOutlined style={{ color: "#215988" }} />
-                  <span className="label-16-500-g-u">Add a New Question</span>
-                </div>
-                <div className="d-flex align-items-center gap-3 mb20">
-                  {/* <PlusOutlined style={{ color: "#215988" }} /> */}
-                  <img
-                    src="./icons/png/add-question.png"
-                    className="height15 width16"
-                  />
-                  <span className="label-16-500-g-u">Add All Questions</span>
-                </div>
-                <div className="d-flex align-items-center gap-3 mb20">
-                  {/* <UploadOutlined style={{ color: "#215988" }} /> */}
-                  <img
-                    src="./icons/png/upload.png"
-                    className="height15 width12"
-                  />
-                  <span className="label-16-500-g-u">Upload Questions</span>
-                </div>
-
-                <div className="d-flex align-items-center gap-3 mb20">
-                  <BankOutlined style={{ color: "#215988" }} />
-                  <span className="label-16-500-g-u">Question Bank</span>
-                </div>
-
-                <div className="d-flex align-items-center gap-3 mb20">
-                  <img
-                    src="./icons/png/ai-green.png"
-                    className="height15 width16"
-                  />
-                  <span className="label-16-500-g-u">Create with AI</span>
-                </div>
-              </div>
-            </Space>
-          </div>
+          <RightSection sections={sections} />
         </Col>
       </Row>
+      {isReviewModalOpen && (
+        <CommonModalComponent
+          open={isReviewModalOpen}
+          setOpen={setReviewModalOpen}
+          modalWidthSize={696}
+          modalHeightSize={599}
+          isClosable={true}
+        >
+          <ReviewModal
+            // CardTitle={"Add Student"}
+            // classroomId={null}
+            closeModal={() => setReviewModalOpen(false)}
+          />
+        </CommonModalComponent>
+      )}
     </div>
   );
 };
---
+++ src/pages/exam_management/ReviewModal.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Button, Row, Col } from "antd";
+// import "./review-modal.css";
+
+export default function ReviewModal({
+  closeModal,
+  //   onReview = () => {},
+}) {
+  const incompleteQuestions = [
+    { number: 19, subject: "Practical Chemistry", marks: 1 },
+    { number: 20, subject: "Practical Chemistry", marks: 1 },
+    { number: 21, subject: "Organic Chemistry", marks: 3 },
+    { number: 22, subject: "Acids & Bases", marks: 4 },
+    { number: 23, subject: "Metals & Compounds", marks: 1 },
+    { number: 24, subject: "Organic Compounds", marks: 1 },
+    { number: 25, subject: "Practical Chemistry", marks: 1 },
+    { number: 26, subject: "Practical Chemistry", marks: 1 },
+    { number: 27, subject: "Practical Chemistry", marks: 1 },
+    { number: 28, subject: "Organic Chemistry", marks: 1 },
+    { number: 29, subject: "Practical Chemistry", marks: 1 },
+    { number: 30, subject: "Practical Chemistry", marks: 1 },
+  ];
+
+  return (
+    // <Modal
+    //   open={open}
+    //   onCancel={onClose}
+    //   footer={null}
+    //   width={600}
+    //   closeIcon={<span className="modal-close">×</span>}
+    // >
+    <div className="review-modal">
+      <div className="modal-content">
+        <div className="modal-title">
+          Please note that the question paper is incomplete.
+        </div>
+        <div className="modal-title">
+          Would you like to proceed with reviewing the paper?
+        </div>
+        <div className="modal-subtitle"></div>
+
+        <div className="incomplete-section">
+          <p className="section-title">
+            The following questions are incomplete
+          </p>
+
+          <Row gutter={[16, 16]}>
+            {incompleteQuestions.map((question, index) => (
+              <Col span={12} key={question.number}>
+                <div className="question-item">
+                  <div className="question-number ">{question.number}</div>
+                  <div className="question-subject">{question.subject}</div>
+                  <div className="eclips-mark"></div>
+                  <div className="label-10-500-italic">
+                    {question.marks} mark{question.marks > 1 ? "s" : ""}
+                  </div>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </div>
+
+        <div className="modal-actions">
+          <Button className="review-button label-18-700-grey">
+            Review Paper
+          </Button>
+          <Button className="cancel-button" onClick={() => closeModal()}>
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </div>
+    // </Modal>
+  );
+}
