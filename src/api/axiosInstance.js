d32d3ca73a77bdf6ce411f638b498114827ecd2d
Chandan <cy2001435@gmail.com>
2025-01-27 13:53:40 +0530

    added v1 in end url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://178.16.139.242:8080/api", // main port
-  baseURL: "http://194.164.149.125:8070/api", // dev port
+  baseURL: "http://194.164.149.125:8070/api/v1", // dev port
   // baseURL: "http://localhost:8080/api/v1", // local port
   timeout: 10000,
 });
